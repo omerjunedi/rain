95c20f13733070c70c97a8d40ee9362e31c828da
ojunedi <ojunedi@gmail.com>
2023-05-24 00:36:47 -0400

    error tracking + confirmation message

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # set up credentials and api call
 import requests
 import random
-from secret import API_KEY, CITY_ID, CITY_NAME, ACCOUNT_SID, AUTH_TOKEN
+from secret import API_KEY, CITY_ID, CITY_NAME, ACCOUNT_SID, AUTH_TOKEN, SHAWTY_BAES_NUMBER, MY_TWILIO_NUMBER, CONFIRMATION_NUMBER
 from twilio.rest import Client
 BASE_URL: str = "https://api.openweathermap.org/data/2.5/weather?id="
 
@@ -34,13 +34,21 @@ def api_call():
 
 def send_message(forecast: str):
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    quote, name = select_quote_and_name()
+    quote, nickname = select_quote_and_name()
     message = client.messages.create(
-        body=f"\n{quote}Please consider taking an umbrella. [Forecast in {CITY_NAME}: {forecast}]\nLove you {name}",
-        from_="+18444410160",
-        to="+12488803467"
+        body=f"\n{quote}Please consider taking an umbrella. [Forecast in {CITY_NAME}: {forecast}]\nI like you a lot {nickname} :)",
+        from_=MY_TWILIO_NUMBER,
+        to=SHAWTY_BAES_NUMBER
     )
-    print(message.sid)
+    if message.status == "sent":
+        confimation_message = client.messages.create(
+            body=f"Message sent to SC: {message.body}\n Date: {message.date_sent}",
+            from_=MY_TWILIO_NUMBER,
+            to=CONFIRMATION_NUMBER
+        )
+    else:
+        with open("errors.txt", 'a') as f:
+            f.write(f"{message.error_code} | {message.error_message} | {message.date_created}")
 
 def main():
     api_call()
